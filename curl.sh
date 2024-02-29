7a481c0a1fa2d380e2c885a21940a84e53b6154c
Your Name <you@example.com>
2024-02-29 02:26:48 +0100

    keys

--- curl.sh
+++ curl.sh
@@ -1,6 +1,6 @@
 curl https://api.openai.com/v1/images/generations \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer key" \
   -d '{
     "model": "dall-e-3",
     "prompt": "A cute baby sea otter",
